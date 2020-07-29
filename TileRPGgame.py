1bfb742edfe824e30099b051b5e464b8ce259502
Harry <Harry.Randall@student.tsc.nsw.edu.au>
2020-07-29 11:44:06 +1000

    Upload

--- TileRPGgame.py
+++ TileRPGgame.py
@@ -1,90 +1,97 @@
-import random
-import os
-import time
-words = ['hi','bye','hahahah','catch','romantic','roof','wander','brainstorm','wage']
-words_with_muti_player = ['']
-
-death_1 = '\n\n\n\n\n__________'
-death_2 = '     |\n     |\n     |\n     |\n____________'
-death_3 = '   __________    \n     |\n     |\n     |\n     |\n____________'
-death_4 = '   __________    \n     | /\n     |/\n     |\n     |\n____________'
-death_5 = '   __________    \n     | /   |\n     |/    |\n     |\n     |\n____________'
-death_6 = '   __________    \n     | /   |\n     |/    O\n     |    \n     |     \n____________'
-death_7 = '   __________    \n     | /   |\n     |/    O\n     |    \/ \n     |     \n____________'
-death_8 = '   __________    \n     | /   |\n     |/    O \n     |    \/ \n     |    /\ \n____________'
+import sys
+import pygame as pg
 
 
-
-
-def Hangman_start():
-    os.system('clear')
-    word = random.choice(words)
-    correct_guesses = ''
-    incorrect_guesses = ''
-    turns = 10
+width, height = 1440, 880
+hbox, vbox = 20, 20
+red = (255,0,0)
+green = (50,205,50)
+white = (255,255,255)
+screen = pg.display.set_mode((width, height))
+clock = pg.time.Clock()
+rect = pg.Rect(300, 220, hbox, vbox)
+velocity = (0, 0)
+game_1_stock_1 = str(rect.x),(rect.y)
+Travelers_lounge_background = pg.image.load("Image.png")
 
 
 
-    while turns > 0:
-        failed = 0
-        for char in word:
-            if char in correct_guesses:
-                print(char)
+def textBoxBlack(text, size, position, color):
+    font = pg.font.Font('freesansbold.ttf', size)
+    text = font.render(text, True, color, (255,0,0))
+    textRect = text.get_rect()
+    textRect.center = position
+    screen.blit(text, textRect)
 
-            else:
-                print("-")
-                failed += 1
-
-        if failed == 0:
-            print("You Win")
-            print("The word is: ", word)
-            break
+def draw_the_map():
+    pg.draw.rect(screen,red,(0,0,1440,15))
 
 
+def main():
+    global box_colour
+    myfont=pg.font.SysFont("freesansbold.ttf", 20)
+    done = False
+    colour = False
+    box_colour = red
 
 
+    while not done:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                done = True
 
-        guess = input("guess a character:")
-        words_in_guess = len(guess)
-        correct_guesses += guess
+        keys = pg.key.get_pressed()
 
-        if guess not in "abcdefghijklmnopqrstuvwxyz":
-            print("not in the alphabet, try again: ")
-        elif guess in incorrect_guesses:
-            os.system('clear')
-            print(incorrect_guesses)
-            print("You have allready guessed that, try again: ")
-        elif words_in_guess >= 2:
-            os.system('clear')
-            print("Please only enter one letter at a time: ")
+        # booster
+        move = 18 if keys[pg.K_LSHIFT] else 4
+
+
+        if keys[pg.K_a]:  #to move left
+            rect.x -= move
+        if rect.x < 0:
+            rect.x = 0
+        if keys[pg.K_d]: #to move right
+            rect.x += move
+
+        if rect.x > width-hbox:
+            rect.x = width - hbox
+        if rect.x > 0 and rect.y > -5 and rect.x < 1440 and rect.y < 15:
+            colour = True
         else:
+            colour = False
 
-            if guess not in word:
-                turns -= 1
-                incorrect_guesses += guess + ","
-                os.system('clear')
-                print("Wrong, here are your incorrect guesses: " + incorrect_guesses)
+        if keys[pg.K_w]:  #to move up
+            rect.y -= move
+        if rect.y < 0: rect.y = 0
 
-                print("You have", + turns, 'more guesses')
-                if turns == 1:
-                    print(death_8)
-                elif turns == 2:
-                    print(death_7)
-                elif turns == 3:
-                    print(death_6)
-                elif turns == 4:
-                    print(death_5)
-                elif turns == 5:
-                    print(death_4)
-                elif turns == 6:
-                    print(death_3)
-                elif turns == 7:
-                    print(death_2)
-                elif turns == 8:
-                    print(death_1)
-                elif turns == 0:
-                    print("You lost")
+        if keys[pg.K_s]: #to move down
+            rect.y += move
+        if rect.y > height - hbox:
+            rect.y = height - vbox
 
 
 
-Hangman_start()
+        if colour == True:
+            box_colour = green
+        if colour == False:
+            box_colour = red
+
+        screen.fill((0, 0, 0))
+        screen.blit(Travelers_lounge_background, (0, 0))
+        draw_the_map()
+        pg.display.update()
+        pg.draw.rect(screen, (box_colour), rect)
+        pg.draw.rect(screen, (box_colour), rect)
+
+
+        pg.display.flip()
+        clock.tick(30)
+        
+
+
+if __name__ == '__main__':
+
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
